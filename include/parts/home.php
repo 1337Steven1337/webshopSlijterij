2f510c5bdc637a462a4e0e650163b8d426d7f01d
Bart Bongers <bartbongers1998@gmail.com>
2017-10-27 23:09:02 +0200

    Alle producten worden ingeladen op de homepage

--- include/parts/home.php
+++ include/parts/home.php
@@ -1,25 +1,18 @@
 <div class="container homeContainer content">
-    <div class="row">
-        <div class="col-md-4 productItem">
-            <h2>Captain Morgan Spiced Gold</h2>
-            <img width="auto" height="325px" src="/include/img/drank1.jpg">
-            <p><a class="btn btn-secondary" href="#" role="button">Meer informatie</a></p>
-        </div>
-        <div class="col-md-4 productItem">
-            <h2>Aberlour</h2>
-            <img width="auto" height="325px" src="/include/img/drank2.jpg">
-            <p><a class="btn btn-secondary" href="#" role="button">Meer informatie</a></p>
-        </div>
-        <div class="col-md-4 productItem">
-            <h2>The Balvenie</h2>
-            <img width="auto" height="325px" src="/include/img/drank3.jpg">
-            <p><a class="btn btn-secondary" href="#" role="button">Meer informatie</a></p>
-        </div>
-    </div>
     <?php
     $products = selectQuery("SELECT name, img FROM product");
-    foreach($products as $row){
-        
+    foreach($products as $row)
+    {
+        if($row["img"] == null){
+            $row["img"] = "noimg.jpg";
+        }
+        echo '
+        <div class="col-md-4 productItem">
+            <h2>'.$row["name"].'</h2>
+            <img width="auto" height="325px" src="/files/product/'.$row["img"].'">
+            <p><a class="btn btn-secondary" href="#" role="button">Meer informatie</a></p>
+        </div>
+        ';
     }
     ?>
 </div>
